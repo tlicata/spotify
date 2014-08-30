29ee881652b51b4cae37873f14990afea17413ea
Tim Licata <tim@timothylicata.com>
2014-08-29 17:35:03 -0700

    search_for_track

--- app/helpers/spotify_helper.rb
+++ app/helpers/spotify_helper.rb
@@ -4,26 +4,27 @@ require "typhoeus"
 module SpotifyHelper
   BASE_URL = "https://api.spotify.com/v1/"
 
-  def SpotifyHelper.search_for_album(query)
+
+  def SpotifyHelper.search_for(query, type)
     results = Typhoeus.get(BASE_URL + "search/",
                            :params => {
                              :q => query,
-                             :type => "album"
+                             :type => type
                            })
-
     body = JSON.parse(results.body)
-    albums = body["albums"]["items"]
-    names = albums.map {|i| i["name"]}
+    items = body[type+"s"]["items"]
+    names = items.map {|i| i["name"]}
+  end
+
+  def SpotifyHelper.search_for_album(query)
+    SpotifyHelper.search_for(query, "album")
   end
 
   def SpotifyHelper.search_for_artist(query)
-    results = Typhoeus.get(BASE_URL + "search/",
-                           :params => {
-                             :q => query,
-                             :type => "artist"
-                           })
-    body = JSON.parse(results.body)
-    artists = body["artists"]["items"]
-    names = artists.map {|i| i["name"]}
+    SpotifyHelper.search_for(query, "artist")
+  end
+
+  def SpotifyHelper.search_for_track(query)
+    SpotifyHelper.search_for(query, "track")
   end
 end
