a8d9bfc872083670e30e71c58d327ad4b1c386be
Tim Licata <tim@timothylicata.com>
2014-08-29 17:19:20 -0700

    search_for_artist

--- app/helpers/spotify_helper.rb
+++ app/helpers/spotify_helper.rb
@@ -15,4 +15,15 @@ module SpotifyHelper
     albums = body["albums"]["items"]
     names = albums.map {|i| i["name"]}
   end
+
+  def SpotifyHelper.search_for_artist(query)
+    results = Typhoeus.get(BASE_URL + "search/",
+                           :params => {
+                             :q => query,
+                             :type => "artist"
+                           })
+    body = JSON.parse(results.body)
+    artists = body["artists"]["items"]
+    names = artists.map {|i| i["name"]}
+  end
 end
