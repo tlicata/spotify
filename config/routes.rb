d14555cb28ac12907fbc401ac20c36c8b997b440
Tim Licata <tim@timothylicata.com>
2014-08-31 09:42:14 -0700

    rails g controller playlists

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   get 'session/callback'
   post 'session/logout'
 
-  resources :users
+  resources :users do
+    resources :playlists
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
