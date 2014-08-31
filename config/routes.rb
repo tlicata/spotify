89c90723e6df256ffd30c1d8407da9fc2689464a
Tim Licata <tim@timothylicata.com>
2014-08-30 20:44:04 -0700

    rails g model user rails g controller users

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'session/callback'
   post 'session/logout'
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
