783632035018bcf25f87e2e1524d41088d350ad4
Tim Licata <tim@timothylicata.com>
2014-09-01 19:42:29 -0700

    if #show-ing current_user, then get their tracks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < SpotifyController
     @user_id = params[:id]
     @body = get_user_profile(@user_id)
     @playlists = get_user_playlists(@user_id)
+    if @user_id == session[:current_user]
+      @tracks = get_user_tracks
+    end
   end
 end
